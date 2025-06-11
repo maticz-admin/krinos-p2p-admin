eb76f6e247ef472b47415d6be4cff431f99a5ffd
sakthivel <sakthivelmaticz@gmail.com>
2025-06-11 11:00:40 +0530

    bugs

--- admin/src/components/pages/Newsletter.js
+++ admin/src/components/pages/Newsletter.js
@@ -43,7 +43,7 @@ const Newsletter = () => {
         }));
         if (!isEmpty(selectedOption)) setErrors({});
     };
-    
+
 
     const handleSelectAll = () => {
         setFormValue((prevFormValue) => ({
@@ -51,7 +51,7 @@ const Newsletter = () => {
             subscribedId: subscribed.map((el) => el.value),
         }));
     };
-    
+
 
     const handleEditorChange = (e) => {
         const message = e.editor.getData();
@@ -61,7 +61,7 @@ const Newsletter = () => {
         }));
         if (!isEmpty(message)) setErrors({});
     };
-    
+
 
     const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -157,7 +157,6 @@ const Newsletter = () => {
                                                 className="border_blue_select basic-multi-select"
                                                 classNamePrefix="select w-full"
                                             />
-                                            <span className="text-danger">{errors.email}</span>
                                         </div>
                                         <button
                                             className="themebtn noshrink"
@@ -167,6 +166,7 @@ const Newsletter = () => {
                                             Select All
                                         </button>
                                     </div>
+                                    <p className="text-danger mt-1">{errors.email}</p>
                                 </div>
                             </div>
 
@@ -175,7 +175,6 @@ const Newsletter = () => {
                                     <label htmlFor="content">Message</label>
                                 </div>
                                 <div className="col-md-9">
-                                    <span className="text-danger">{errors.message}</span>
                                     <CKEditor
                                         config={{
                                             extraAllowedContent: "div(*)",
@@ -185,6 +184,8 @@ const Newsletter = () => {
                                         initData={formValue.message}
                                         onChange={handleEditorChange}
                                     />
+                                    <p className="text-danger mt-1">{errors.message}</p>
+
                                 </div>
                             </div>
                         </form>
